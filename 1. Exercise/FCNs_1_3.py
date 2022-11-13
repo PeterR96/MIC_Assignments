7bca58c7cfbda5274e85ebbb6e8e0f0f358e158a
PeterR96 <rott_peter@yahoo.de>
2022-11-13 21:42:54 +0100

    sobel

--- 1. Exercise/FCNs_1_3.py
+++ 1. Exercise/FCNs_1_3.py
@@ -12,24 +12,26 @@ from matplotlib import pyplot as plt
 
 def Sobel_kernel(img):
     
-    lap = cv2.Laplacian(img,cv2.CV_32F) 
-    sobelx = cv2.Sobel(img,ddepth=cv2.CV_32F,dx=1,dy=0)  # x
-    sobely = cv2.Sobel(img,ddepth=cv2.CV_32F,dx=0,dy=1)  # y
+    sobelx = cv2.Sobel(img,ddepth=cv2.CV_32F,dx=1,dy=0,ksize=3)  # x
+    sobely = cv2.Sobel(img,ddepth=cv2.CV_32F,dx=0,dy=1,ksize=3)  # y
     sobel = [sobelx,sobely]
     cv2.imwrite('Sobel_horizontal.tif', sobelx)
     cv2.imwrite('Sobel_vertical.tif', sobely)
-    cv2.imwrite('Laplacian.tif', lap)
+ 
     
     return sobel
 
-def Img_Gradient(sobel):
+def Img_Gradient(sobel,thresh=(0, 65535)):
     
     mag = np.sqrt((sobel[0] ** 2) + (sobel[1] ** 2))
-    orientation = np.arctan2(sobel[1], sobel[0]) * (180 / np.pi) % 180
-    #cv2.imshow("gradient magnitude", mag)
     
+    scale_factor = np.max(mag)/65535
+    mag = (mag/scale_factor).astype(np.uint16) 
+    # create a binary image of ones where threshold is met, zeros otherwise
+    binary_output = np.zeros_like(mag)
+    binary_output[(mag >= thresh[0]) & (mag <= thresh[1])] = 1
+    # return the binary image
     cv2.imwrite('Gradiant_Magnetut.tif', mag)
-    cv2.imwrite('Orientation.tif', orientation)
-    
+    cv2.imwrite('Gradiant_Magnetut_Threshhold.tif', binary_output)
+    return binary_output
     return mag
-    return orientation
