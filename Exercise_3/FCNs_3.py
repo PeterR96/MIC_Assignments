e75583db26023a6fd5276ca0f87e93bd90b3d49c
PeterR96 <rott_peter@yahoo.de>
2023-02-03 15:49:30 +0100

    ex3 new

--- Exercise_3/FCNs_3.py
+++ Exercise_3/FCNs_3.py
@@ -9,7 +9,7 @@ import numpy as np
 
 def extract_test_data (design_matrix):
     
-    first_row = design_matrix[:53,:]
+    first_row = design_matrix[:52,:]
     last_52_rows = design_matrix [-52:, :]
     
     test_data = np.vstack ((first_row, last_52_rows))
@@ -19,7 +19,7 @@ def extract_test_data (design_matrix):
 def extract_train_data (design_matrix):
     
     first_row = design_matrix[:1,:]
-    rows_2_to_624 = design_matrix[53:625, :]
+    rows_2_to_624 = design_matrix[52:625, :]
     
     train_data = np.vstack ((first_row, rows_2_to_624))
     
